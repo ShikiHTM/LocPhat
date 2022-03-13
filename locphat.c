8a7386d0e13e25ebf95f2164dcc41dd3a7e7443e
ShikiHTM <90792678+ShikiHTM@users.noreply.github.com>
2022-03-14 06:11:49 +0700

    Create locphat.c

---
+++ locphat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <conio.h>
+
+#define ll long long
+#define ar array
+
+int m,k,i=0;
+
+int main() 
+{
+	// FILE *nt;
+	int n;
+	scanf("%d",&n);
+	printf("Length: %d", dodai);
+	while(n>0){
+		m=*n;
+		k=*n%10;
+		if(k==0||k==6||k==8){
+			m=0;
+		}
+		else
+		i=1;
+		break;	
+	}
+	if(i==1)
+	printf("0");
+	else printf("1");
+}
